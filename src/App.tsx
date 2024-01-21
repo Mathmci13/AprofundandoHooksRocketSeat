79f1aa77fb25a2c3b6648361ae8ba0bd5a0eeec7
Mathmci13 <mathmci002@gmail.com>
2024-01-20 22:05:22 -0300

    Pagina Home

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from './styles/global'
 import { BrowserRouter } from 'react-router-dom'
 import { Router } from './Router'
 
-// ultima aula assistida: Header e Layout, modulo 2
+// ultima aula assistida: Pagina: Home, modulo 2
 
 export function App() {
   return (
