ea09fc04f899067864e586abd3b32929cf64ac99
Mathmci13 <mathmci002@gmail.com>
2024-02-01 20:59:39 -0300

    Finalizado módulo 3

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,12 +9,36 @@ import {
   TaskInput,
 } from './styles'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import zod from 'zod'
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, 'Informe a tarefa'),
+  minutesAmount: zod
+    .number()
+    .min(5, 'Ciclo mínimo de 5 minutos!')
+    .max(60, 'Ciclo máximo de 60 minutos!'),
+})
+
+// interface NewCycleFormData {
+//   task: string
+//   minutesAmount: number
+// }
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 export function Home() {
-  const { register, handleSubmit, watch } = useForm()
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    },
+  })
 
-  function HandleSubmit(data: any) {
+  function HandleSubmit(data: NewCycleFormData) {
     console.log(data)
+    reset()
   }
 
   const task = watch('task')
